2e79ac693c2b82edd0e5e7dd22fc71ecf439f053
Hippo <hippolyte.geslain@laplateforme.io>
2025-02-11 10:48:22 +0100

    midday commit

--- Combat.py
+++ Combat.py
@@ -1,6 +1,19 @@
+import Pokemon, random
+
+type_table = 'type_chart.json'
+
 class Combat():
     def __init__(self, pokemon1, pokemon2):
-        self.pokemon1 = pokemon1
-        self.pokemon2 = pokemon2
-    def dmg_calculus(self):
-        self.pokemon1.
+        self.pokemon_1 = pokemon1
+        self.pokemon_2 = pokemon2
+        self.type_table = type_table
+    def type_multiplicator(self):
+        multiplicator = self.type_table[self.pokemon_1.type][self.pokemon_2.type]
+        return multiplicator
+    def random_multiplicator(self):
+        random = random.randint(217,255)
+        return random
+    def power_calculator(self):
+        power = 1
+    def dmg_calculator(self):
+        self.pokemon_1.attack * self.type_multiplicator() * self.random_multiplicator()
